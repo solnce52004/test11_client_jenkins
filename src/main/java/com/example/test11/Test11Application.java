5a370dfd9566684916b4ba9a2549e357782f002a
solnce52004 <solnce52004@yandex.ru>
2021-12-26 15:18:35 +0300

    <master> try db ok

--- src/main/java/com/example/test11/Test11Application.java
+++ src/main/java/com/example/test11/Test11Application.java
@@ -3,8 +3,10 @@ package com.example.test11;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.yaml")
 public class Test11Application {
     @Value("${spring.datasource.url}")
     private static String url;
@@ -12,9 +14,9 @@ public class Test11Application {
     private static String driver;
 
     public static void main(String[] args) {
-        SpringApplication.run(Test11Application.class, args);
         System.out.println("-------- app started ---------");
         System.out.println("-------- url ---------" + url);
         System.out.println("-------- driver ---------" + driver);
+        SpringApplication.run(Test11Application.class, args);
     }
 }
